124870ccab3e92f66d66b7b8d8679b5853e52a48
nbharat97 <bharatmnair246@gmail.com>
2020-09-13 09:49:50 +0530

    powerset using recursion

---
+++ powerset.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//reference gfg and cs dojo
+void powerset(string str, int index){
+
+    string curr;
+    for (int i = index; i < str.size(); i++)
+    {
+        curr = curr + str[i];
+        cout<<curr<<endl;
+
+    
+    }
+    if(index < str.size()){
+
+        powerset(str, index+1);
+    
+    }
+
+    
+
+}
+int main(){
+
+    //recursive to generate a powerset
+    string str;
+    int index = 0;
+
+    cout<<"enter string"<<endl;
+    cin>>str;
+    //for the empty string
+    cout<<" "<<endl;
+    powerset(str, index);
+    return 0;
+}
